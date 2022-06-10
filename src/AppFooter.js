d5e19f4155bfe379f6f97dd6c43afa94657e9814
shindonghwi <ehdgnl8940.naver.com>
2022-06-10 14:02:53 +0900

    feat: AppFooter 컴포넌트 제작

---
+++ src/AppFooter.js
@@ -0,0 +1,12 @@
+import React, {Component, Fragment} from 'react'
+
+export default class AppFooter extends Component{
+    render() {
+        return (
+            <Fragment>
+                <hr/>
+                <p>Copyright &copy; 2022 ShinDonghwi.</p>    
+            </Fragment>
+        )
+    }
+}
--- src/index.js
+++ src/index.js
@@ -1,12 +1,18 @@
 import React, { Component } from 'react'
 import ReactDom from 'react-dom'
+import AppFooter from './AppFooter'
 
-class App extends Component{
+class App extends Component {
 
   render() {
-    return <div>
-      <h1>Hello World</h1>
-    </div>
+    return (
+      <div>
+        <div>
+          <h1>Hello World</h1>
+        </div>
+        <AppFooter/>
+      </div>
+    )
   }
 }
 
