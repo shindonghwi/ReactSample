3dc0c3da54aaa2f324d6b6b94ddf5f36f2840661
shindonghwi <ehdgnl8940.naver.com>
2022-06-10 15:00:34 +0900

    feat: json 데이터 호출 후 ui에 렌더링 하는 샘플 기능 추가

--- src/AppContent.js
+++ src/AppContent.js
@@ -4,14 +4,30 @@ import React, { Component } from "react"
 
 export default class AppContent extends Component{
 
+    fetchList = () => {
+        fetch('https://jsonplaceholder.typicode.com/posts')
+            .then(response => response.json())
+            .then(json => {
+                console.log(json)
+                let posts = document.getElementById("post-list")
+
+                json.forEach(function(obj){
+                    let li = document.createElement("li")
+                    li.appendChild(document.createTextNode(obj.title))
+                    posts.appendChild(li)
+                })
+            })
+    }
+
     render(){
         return (
             <p>
                 This is the content.
 
                 <br/>
-                <button class="btn btn-primary" href='#'>My Button</button>
+                <button onClick = {this.fetchList} className="btn btn-primary">Fetch Data</button>
 
+                <ul id ="post-list"></ul>
             </p>
         )
     }
